4ee3999da7b5a5cfbf69c29f0d4b93a04d7d3294
Mark Shroyer <mark@shroyer.name>
2025-04-28 22:37:31 -0700

    [tsock] Refactor script
    Also make more consistent use of quoted shell args

--- cfg.bin/tsock.sh
+++ cfg.bin/tsock.sh
@@ -15,28 +15,21 @@ TSOCKDIR="/tmp/tsock-$(id -u)"
 SERVERSDIR="$TSOCKDIR/servers"
 TTYSDIR="$TSOCKDIR/ttys"
 
-UNAME=
-stat_mode() {
-	if [ -z "$UNAME" ]; then
-		UNAME="$(uname)"
-	fi
-	if [ "$UNAME" = "Linux" ]; then
-		stat -c '%a' "$1"
-	else
-		stat -f '%Lp' "$1"
-	fi
+show_usage() {
+cat <<'EOF'
+tsock.sh - Wrangle SSH agent sockets for tmux sessions
+
+Usage:
+    tsock.sh set-tty-link
+    tsock.sh set-server-link [client_tty]
+    tsock.sh show-server-link
+    tsock.sh help
+EOF
 }
 
-ensure_dir() {
-	if [ ! -d $1 ]; then
-		mkdir -m700 $1
-	fi
-	if [ ! -O $1 ]; then
-		echo "expected $1 to be owned by $(id -u)" >&2
-		exit 1
-	fi
-	if [ "$(stat_mode $1)" != 700 ]; then
-		chmod 700 $1
+log() {
+	if [ ! -z "$LOGFILE" ]; then
+		echo "$(date +'%Y-%m-%d %H:%M:%S') $1" >>"$LOGFILE"
 	fi
 }
 
@@ -59,25 +52,36 @@ get_filename_device() {
 	echo "/$(echo $1 | tr + /)"
 }
 
+UNAME=
+stat_mode() {
+	if [ -z "$UNAME" ]; then
+		UNAME="$(uname)"
+	fi
+	if [ "$UNAME" = "Linux" ]; then
+		stat -c '%a' "$1"
+	else
+		stat -f '%Lp' "$1"
+	fi
+}
+
+ensure_dir() {
+	if [ ! -d "$1" ]; then
+		mkdir -m700 "$1"
+	fi
+	if [ ! -O "$1" ]; then
+		echo "expected $1 to be owned by $(id -u)" >&2
+		exit 1
+	fi
+	if [ "$(stat_mode $1)" != 700 ]; then
+		chmod 700 "$1"
+	fi
+}
+
 set_symlink() {
-	if [ -L "$2" ]; then
-		if [ "$(readlink $2)" = "$1" ]; then
-			return
-		else
-			rm "$2"
-		fi
+	if [ -L "$2" ] || [ -e "$2" ]; then
+		rm "$2"
 	fi
 	ln -s "$1" "$2"
-}
-
-get_tty_link_path() {
-	echo "$TTYSDIR/$(get_device_filename $1)"
-}
-
-set_tty_link() {
-	if [ ! -z "$SSH_AUTH_SOCK" ] && [ -O "$SSH_AUTH_SOCK" ]; then
-		set_symlink $SSH_AUTH_SOCK "$(get_tty_link_path $(tty))"
-	fi
 }
 
 # Clean up any of the tty links that no longer both refer to an existing tty
@@ -91,22 +95,21 @@ gc_tty_links() {
 	done
 }
 
-show_usage() {
-cat <<'EOF'
-tsock.sh - Wrangle SSH agent sockets for tmux sessions
-
-Usage:
-    tsock.sh set-tty-link
-    tsock.sh set-server-link [client_tty]
-    tsock.sh show-server-link
-    tsock.sh help
-EOF
+get_tty_link_path() {
+	echo "$TTYSDIR/$(get_device_filename $1)"
 }
 
-ensure_dirs() {
+set_tty_link() {
 	ensure_dir "$TSOCKDIR"
-	ensure_dir "$SERVERSDIR"
 	ensure_dir "$TTYSDIR"
+
+	# Since this will be called infrequently, typically when new SSH
+	# clients connect, it's a good place to GC old symlinks.
+	gc_tty_links
+
+	if [ ! -z "$SSH_AUTH_SOCK" ] && [ -O "$SSH_AUTH_SOCK" ]; then
+		set_symlink "$SSH_AUTH_SOCK" "$(get_tty_link_path $(tty))"
+	fi
 }
 
 get_active_client_tty() {
@@ -120,31 +123,30 @@ get_server_link_path() {
 	echo "$SERVERSDIR/$(tmux list-sessions -F '#{pid}' | head -n1)"
 }
 
-log() {
-	if [ ! -z "$LOGFILE" ]; then
-		echo "$(date +'%Y-%m-%d %H:%M:%S') $1" >>"$LOGFILE"
-	fi
-}
-
 set_server_link() {
 	ttylink="$(get_tty_link_path $1)"
 	serverlink="$(get_server_link_path)"
+
+	# This may be called frequently, as a ZSH hook or periodically, so
+	# let's optimize the happy path where the link is already set
+	# correctly.
 	if [ -L $serverlink ] && [ "$(readlink $serverlink)" = "$ttylink" ]; then
 		return
 	fi
-	ensure_dirs
+
+	ensure_dir "$TSOCKDIR"
+	ensure_dir "$SERVERSDIR"
 	set_symlink "$ttylink" "$serverlink"
 }
 
 if [ "$1" = "-h" ] || [ "$1" = "help" ]; then
 	show_usage
 elif [ "$1" = "set-tty-link" ]; then
-	ensure_dirs
-	gc_tty_links
+	log "$@"
 	set_tty_link
 elif [ "$1" = "set-server-link" ]; then
+	log "$@"
 	shift
-	log "set-server-link $1 $2"
 	if [ -z "$1" ]; then
 		set_server_link "$(get_active_client_tty)"
 	else
