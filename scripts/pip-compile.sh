d98988371123da6d42e8ee7582dd1f705fb415ad
Mark Shroyer <mark@shroyer.name>
2025-08-19 13:16:15 -0700

    Fix requirements.txt generation
    Was accidentally including dev requirements in the main file, which
    broke tests on FreeBSD.

--- scripts/pip-compile.sh
+++ scripts/pip-compile.sh
@@ -3,4 +3,4 @@
 set -e
 
 pip-compile pyproject.toml $@
-pip-compile --extra dev -c requirements.txt pyproject.toml $@
+pip-compile --extra dev -c requirements.txt pyproject.toml $@ -o dev-requirements.txt
