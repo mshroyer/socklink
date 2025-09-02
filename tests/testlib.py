1f41db770d311b74361cd868a746b994bd6eae82
Mark Shroyer <mark@shroyer.name>
2025-09-01 21:02:00 -0700

    [tests] Increase max_wait for stdout files
    This might fix some intermittent flakes, especially on macOS

--- tests/testlib.py
+++ tests/testlib.py
@@ -206,7 +206,7 @@ class Term:
     @staticmethod
     def _read_output(path: Path) -> str:
         poll_interval = timedelta(milliseconds=25)
-        max_wait = timedelta(seconds=2)
+        max_wait = timedelta(seconds=4)
         start = time.monotonic()
 
         while True:
