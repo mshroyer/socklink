5d0d0926f0241ab3b7905bdb9789b3978484f97d
Mark Shroyer <mark@shroyer.name>
2025-09-04 21:43:17 -0700

    [tests] max_wait=21s, yolo

--- tests/testlib.py
+++ tests/testlib.py
@@ -206,7 +206,7 @@ class Term:
     @staticmethod
     def _read_output(path: Path) -> str:
         poll_interval = timedelta(milliseconds=25)
-        max_wait = timedelta(seconds=11)
+        max_wait = timedelta(seconds=21)
         start = time.monotonic()
 
         while True:
