d14bd9c1afa06f76dd4491cd46f86212e56083d5
Mark Shroyer <mark@shroyer.name>
2025-09-26 13:15:09 -0700

    [scripts] Update SDF deploy script
    Change the script to run on the cluster itself, and additionally install
    under /sys/sdf/bin.

--- scripts/deploy_sdf.sh
+++ scripts/deploy_sdf.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# Deploys the checked out version of socklink to SDF.  Should be run on the
+# regular cluster.
+
 set -e
 
 PROJECT=$(cd "$(dirname "$0")/.." && pwd)
 
-scp "$PROJECT/socklink.sh" rie.sdf.org:
+install "$PROJECT/socklink.sh" "$HOME/socklink.sh"
+install "$PROJECT/socklink.sh" /sys/sdf/bin/socklink.sh
 scp "$PROJECT/socklink.sh" ma.sdf.org:
