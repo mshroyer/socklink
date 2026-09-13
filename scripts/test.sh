c068a525890a6326bf509d263577f963429797cf
Mark Shroyer <mark@shroyer.name>
2026-09-13 06:11:57 +0000

    Set --retires=2 on macOS
    This might fix a few more transient test failures

--- scripts/test.sh
+++ scripts/test.sh
@@ -19,7 +19,7 @@ if [ "$(uname)" = "Darwin" ]; then
 	# stdout from shell commands run within tmux in pexpect sessions, so
 	# let's specifically enable retries there to minimize workflow
 	# failures.
-	RETRIES_FLAG='--retries=1'
+	RETRIES_FLAG='--retries=2'
 fi
 
 python_version_at_least() {
